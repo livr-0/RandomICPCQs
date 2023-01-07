041b7dd867d4de9ec1685700fd3ab8cf5fb757b2
Karan Shah <karanshah0206@outlook.com>
2023-01-07 10:53:24 +0530

    bottles works but is too slow

--- Karan/bottles.cpp
+++ Karan/bottles.cpp
@@ -1,32 +1,41 @@
 #include <iostream>
+#include <cmath>
 #include <vector>
 
 using namespace std;
 
-// prints specified polynomial for testing purposes
-void printPolynomial(const vector<double>& poly) {
-  for (size_t i = 0; i < poly.size(); i++) cout << poly[i] << "^" << i << " ";
-  cout << endl;
-}
+// storing polynomials as a vector of doubles
+// (index = degree, value = coefficient)
+typedef vector<double> poly;
 
-// solve a polynomial for a specified x-value
-double solvePolynomial(const vector<double>& polynomial, double x) {
+// return result of substituting specified x-value in a polynomial
+double solvePolynomial(const poly& polynomial, double x) {
   double res = 0.0;
   for (size_t i = 0; i < polynomial.size(); i++) res += polynomial[i] * pow(x, i);
   return res;
 }
 
+// return a root of a given polynomial
+double getRootOfPolynomial(const poly& polynomial, const poly& derivative) {
+  double res = 0.0, guess;
+  do {
+    guess = res;
+    res = guess - solvePolynomial(polynomial, guess) / solvePolynomial(derivative, guess);
+  } while (fabs(res - guess) >= 0.005);
+  return res;
+}
+
 // return a new polynomial integrated with respect to x
-vector<double> integratePolynomial(const vector<double>& polynomial) {
-  vector<double> integral;
-  integral.push_back(0);
-  for (size_t i = 0; i < polynomial.size(); i++) integral.push_back(polynomial[i] / (i + 1));
-  return integral;
+poly getAntiderivative(const poly& polynomial) {
+  poly antiderivative;
+  antiderivative.push_back(0);
+  for (size_t i = 0; i < polynomial.size(); i++) antiderivative.push_back(polynomial[i] / (i + 1));
+  return antiderivative;
 }
 
 // return the square of a polynomial
-vector<double> squarePolynomial(const vector<double>& polynomial) {
-  vector<double> res;
+poly squarePolynomial(const poly& polynomial) {
+  poly res;
   for (size_t i = 0; i <= (polynomial.size() - 1) * 2; i++) res.push_back(0);
   for (size_t i = 0; i < polynomial.size(); i++)
     for (size_t j = 0; j < polynomial.size(); j++)
@@ -35,26 +44,50 @@ vector<double> squarePolynomial(const vector<double>& polynomial) {
 }
 
 // get the volume of the bottle
-double getVolume(const vector<double>& polynomial, double xHigh, double xLow) {
-  vector<double> integral = integratePolynomial(squarePolynomial(polynomial));
-  double area = solvePolynomial(integral, xHigh) - solvePolynomial(integral, xLow);
+double getVolume(const poly& antiderivative, double xHigh, double xLow) {
+  double area = solvePolynomial(antiderivative, xHigh) - solvePolynomial(antiderivative, xLow);
   return area * 2 * acos(0.0);
 }
 
+// find upper bound that yields specified volume given a polynomial, its derivative, and the lower bound
+double getUpperBound(poly polynomial, const poly& derivative, double volume, double xLow) {
+  polynomial[0] = -(volume / (2 * acos(0.0)) + solvePolynomial(polynomial, xLow));
+  return getRootOfPolynomial(polynomial, derivative);
+}
+
 int main() {
-  size_t t = 1;
+  cout << fixed; cout.precision(2); // values rounded to 2 decimal places
+  size_t t = 1; // stores test case index
   while (1) {
-    vector<double> polynomial;
-    size_t order, inc;
-    double xHigh, xLow;
+    poly polynomial, polySquared, antiderivative;
+    vector<double> heightMarks;
+    double xHigh, xLow, volume, lastBound = 0.0;
+    size_t order, volumeIncrement;
 
+    // get input
     cin >> order;
-    for (size_t i = 0; i <= order; i++) { double x; cin >> x; polynomial.push_back(x); }
-    cin >> xLow >> xHigh >> inc;
-    double volume = getVolume(polynomial, xHigh, xLow);
+    for (size_t i = 0; i <= order; i++) {
+      double x; cin >> x;
+      polynomial.push_back(x);
+    }
+    cin >> xLow >> xHigh >> volumeIncrement;
 
-    cout << fixed; cout.precision(2);
+    // process output
+    polySquared = squarePolynomial(polynomial);
+    antiderivative = getAntiderivative(polySquared);
+    volume = getVolume(antiderivative, xHigh, xLow);
+    while (heightMarks.size() < 8) {
+      double guessVal = ((xHigh - xLow) / 8) * (heightMarks.size() + 1);
+      lastBound = getUpperBound(antiderivative, polySquared, volumeIncrement, lastBound);
+      if (lastBound >= ((xHigh - xLow) - 0.01)) break;
+      heightMarks.push_back(lastBound);
+    }
+
+    // display output
     cout << "Case " << t++ << ": " << volume << endl;
+    if (heightMarks.size() == 0) cout << "insufficient volume";
+    else for (const double& h : heightMarks) cout << h << " ";
+    cout << endl;
   }
   return 0;
 }
