25dd96e530e44ee4e2d1ced3cdf9b261b90243f7
Karan Shah <karanshah0206@outlook.com>
2023-01-19 12:01:46 +0530

    Posterize Written Better

--- Karan/posterize.cpp
+++ Karan/posterize.cpp
@@ -1,45 +1,64 @@
 #include <iostream>
-#include <cstring>
-
-#define LARGE_VAL 1e18
+#include <vector>
 
 using namespace std;
 
-long long nv[256], cs[257][257], m[256][256];
+vector<size_t> redVals(256);
+vector<vector<size_t>> computedSums;
+vector<vector<long long>> memoizedVals;
 
-long long dp(int p, int k, int cur) {
+void initialiseVectors() {
+  for (size_t i = 0; i <= 256; i++) {
+    vector<size_t> cs(257);
+    computedSums.push_back(cs);
+
+    if (i != 256) {
+      vector<long long> m(256);
+      for (long long& a : m) a = -1;
+      memoizedVals.push_back(m);
+    }
+  }
+}
+
+size_t getInput() {
+  size_t d, k;
+  cin >> d >> k;
+
+  while (d-- > 0) {
+    size_t r, p;
+    cin >> r >> p;
+    redVals[r] = p;
+  }
+
+  return k;
+}
+
+void computeSums() {
+  for (size_t i = 0; i <= 256; i++)
+    for (size_t j = 0; j < 256; j++)
+      computedSums[i][j + 1] =  computedSums[i][j] + (i - j) * (i - j) * redVals[j];
+}
+
+long long dp(size_t k, size_t p = 0, size_t c = 0) {
   if (p == 256) return 0;
-  if (k == 0) return LARGE_VAL;
+  if (k == 0) return 1e18;
 
-  long long& res = m[p][k];
+  long long& res = memoizedVals[p][k];
   if (res != -1) return res;
-  res = LARGE_VAL;
+  res = 1e18;
 
-  for (int i = p + 1; i <= 256; i++) {
-    while (cs[cur + 1][i] - cs[cur + 1][p] < cs[cur][i] - cs[cur][p]) cur++;
-    res = min(res, cs[cur][i] - cs[cur][p] + dp(i, k - 1, cur));
+  for (size_t newP = p + 1; newP <= 256; newP++) {
+    while (computedSums[c + 1][newP] - computedSums[c + 1][p] < computedSums[c][newP] - computedSums[c][p]) c++;
+    res = min(res, (long long)(computedSums[c][newP] - computedSums[c][p] + dp(k - 1, newP, c)));
   }
 
   return res;
 }
 
 int main() {
-  int d, k;
-  cin >> d; cin >> k;
-
-  memset(nv, 0, sizeof(nv));
-  memset(m, -1, sizeof(m));
-
-  while (d-- > 0) {
-    size_t r, p;
-    cin >> r >> p;
-    nv[r] += p;
-  }
-
-  for (size_t i = 0; i < 257; i++)
-    for (size_t j = 0; j < 256; j++)
-      cs[i][j + 1] = cs[i][j] + (i - j) * (i - j) * nv[j];
-
-  cout << dp(0, k, 0) << endl;
+  initialiseVectors();
+  size_t k = getInput();
+  computeSums();
+  cout << dp(k) << endl;
   return 0;
 }
