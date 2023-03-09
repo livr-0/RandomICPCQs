849134b5dab26f39206208eb14f57a9ab73f736a
Karan Shah <karanshah0206@outlook.com>
2023-03-09 16:28:51 +1100

    Jousting Tournament

---
+++ Divisionals South Pacific 2016/J.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <stack>
+#include <string>
+#include <set>
+
+using namespace std;
+
+stack<string> competitors;
+string lastChar;
+
+void initialiseVariables() {
+    size_t n;
+    cin >> n;
+    for (; n > 0; n--) competitors.push(to_string(n));
+    lastChar = "c";
+}
+
+void printAndPopTwo() {
+    cout << competitors.top() << " ";
+    competitors.pop();
+    cout << competitors.top() << " ";
+    competitors.pop();
+}
+
+void joust() {
+    if (competitors.size() == 2) {
+        printAndPopTwo();
+        cout << "a" << endl;
+    } else {
+        printAndPopTwo();
+        if (lastChar == "b") {
+            cout << "c" << endl;
+            lastChar = "c";
+        } else {
+            cout << "b" << endl;
+            lastChar = "b";
+        }
+        competitors.push(lastChar);
+    }
+}
+
+int main() {
+    initialiseVariables();
+    while (!competitors.empty()) joust();
+    return 0;
+}
