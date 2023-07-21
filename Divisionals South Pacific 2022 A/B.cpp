2a5005890a2d3ffe6807747b382953dbb537cf89
Linh <63531773+livr-0@users.noreply.github.com>
2023-07-21 15:12:32 +1000

    solved bubble bath sort

--- Divisionals South Pacific 2022 A/B.cpp
+++ Divisionals South Pacific 2022 A/B.cpp
@@ -1,45 +1,76 @@
 #include <iostream>
 #include <vector>
+#include <unordered_map>
+#include <set>
 #include <algorithm>
 
 using namespace std;
 
+void merge(unordered_map<size_t, size_t>& table, vector<size_t>& out, vector<size_t>& left, vector<size_t>& right) {
+    size_t i = 0, j = 0, k = 0;
+    while (i < left.size() && j < right.size()) {
+        if (left[i] > right[j]) {
+            table[right[j]] += (left.size() - i);
+            out[k++] = right[j++];
+        }
+        else {
+            out[k++] = left[i++];
+        }
+    }
+    while(i < left.size()) {
+        out[k++] = left[i++];
+    }
+    while(j < right.size()) {
+        out[k++] = right[j++];
+    }
+
+}
+
+void mergeSort(unordered_map<size_t, size_t>& table, vector<size_t>& in) {
+    if (in.size() == 1) return;
+    vector<size_t> left(in.begin(), in.begin() + in.size() / 2);
+    mergeSort(table, left);
+    vector<size_t> right(in.begin() + in.size() / 2, in.end());
+    mergeSort(table, right);
+    merge(table, in, left, right);
+}
+
 int main() {
     size_t n;
     cin >> n;
+    unordered_map<size_t, size_t> table;
+    vector<size_t> list;
     size_t s;
-    vector<size_t> sortNeeded;
-
-    for (size_t i = 0; i < n; i++) {
+    for(size_t i = 0; i < n; i++) {
         cin >> s;
-        sortNeeded.push_back(s);
+        list.push_back(s);
+        table.emplace(s, 0);
     }
 
-    size_t tCount = 0;
-    vector<size_t> sCounts;
-    size_t sCount = 0;
+    mergeSort(table, list);
+    vector<size_t> counts;
+    for (auto& t : table) {
+        counts.push_back(t.second);
+    }
+    sort(counts.begin(), counts.end());
 
-    while (n--) {
-        for (size_t i = 0; i < sortNeeded.size() - 1; i++) {
-            if (sortNeeded[i] > sortNeeded[i+1]) {
-                swap(sortNeeded[i], sortNeeded[i+1]);
-                sCount++;
-            }
+    vector<size_t> swaps;
+    size_t sub = 0;
+    for(size_t i = 0; i < n; i++) {
+        if (counts[i] == 0) {
+            if (sub > 0) break;
+            continue;
         }
-
-        if (sCount > 0) {
-            sCounts.push_back(sCount);
-            sCount = 0;
-            tCount++;
-        }
-        else {
-            break;
+        while (counts[i] > sub) {
+            swaps.push_back(n - i);
+            sub++;
         }
     }
 
-    cout << tCount << '\n';
-
-    for (auto& s : sCounts) {
-        cout << s << '\n';
+    cout << sub << endl;
+    for (auto& s : swaps) {
+        cout << s << endl;
     }
+
+    return 0;
 }
