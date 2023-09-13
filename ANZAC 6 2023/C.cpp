60e5dcc4762d7a790e71b829850691771320afc2
Karan Shah <62107454+karanshah0206@users.noreply.github.com>
2023-09-13 18:48:42 +1000

    C: Cup Covering

---
+++ ANZAC 6 2023/C.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    double a;
+    cin >> a;
+    double r = sqrt(a / M_PI);
+    cout.precision(10);
+    cout << fixed << r * 2 << endl;
+    return 0;
+}
